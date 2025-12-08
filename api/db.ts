7390cfd4a0c5a69e2c9dc223fd5ddeaa98d8881c
i7620-lgtm <i7620@guru.sd.belajar.id>
2025-12-08 09:53:58 +0800

    Update db.ts

--- api/db.ts
+++ api/db.ts
@@ -1,5 +1,9 @@
 import { Pool } from 'pg';
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL environment variable is missing");
+}
+
 // Connection String will be provided via Vercel Environment Variables
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
