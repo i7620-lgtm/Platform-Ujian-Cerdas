5c81f5f228554177ba89d72fe4b8a3f1c2082545
i7620-lgtm <i7620@guru.sd.belajar.id>
2026-01-28 23:58:07 +0800

    Update Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 export const LogoIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
@@ -22,7 +21,7 @@ export const CalculatorIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) =
 
 export const CloudArrowUpIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
     <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l-3.75 3.75M12 9.75l3.75 3.75m-7.5 4.5V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.25 15.75V12.75c0-1.422-.832-2.65-2.012-3.143-1.1-2.25-3.41-3.6-5.988-3.6-2.72 0-5.082 1.5-6.237 3.69-1.3.36-2.263 1.5-2.263 2.813v2.531m16.5 0h-16.5" />
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l-3 3m3-3l3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
     </svg>
 );
 
