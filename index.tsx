27e642d7bb91b6ff007717385ef6120d5a0020ea
i7620-lgtm <i7620@guru.sd.belajar.id>
2026-01-29 19:04:18 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './style.css'; // Mengimpor CSS di sini agar diproses oleh Tailwind & Vite
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -14,4 +14,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-   
