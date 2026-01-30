019dbaad67b4d732f30f7756a7f5ad4d4814e07c
i7620-lgtm <i7620@guru.sd.belajar.id>
2026-01-30 19:25:15 +0800

    Delete Lib/supabase.ts

--- Lib/supabase.ts
+++
@@ -1,8 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js';
-
-// Menggunakan kredensial yang Anda berikan
-const supabaseUrl = 'https://fidxmkjpdlfawagawrrm.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpZHhta2pwZGxmYXdhZ2F3cnJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3NDgzNjksImV4cCI6MjA4NTMyNDM2OX0.X-ln0rn5tpGgrCpPxiv-FkfPuvAAmORdNRaqwa9RetM';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
