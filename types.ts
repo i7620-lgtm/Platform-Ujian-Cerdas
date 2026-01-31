da6362626a62eb02a0037b750002762546edc7d8
i7620-lgtm <i7620@guru.sd.belajar.id>
2026-01-31 12:18:43 +0800

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,4 +1,4 @@
- 
+
 export type QuestionType = 'MULTIPLE_CHOICE' | 'COMPLEX_MULTIPLE_CHOICE' | 'TRUE_FALSE' | 'MATCHING' | 'ESSAY' | 'FILL_IN_THE_BLANK' | 'INFO';
 
 export interface Question {
@@ -35,6 +35,7 @@ export interface ExamConfig {
   showResultToStudent: boolean;
   showCorrectAnswer: boolean;
   enablePublicStream: boolean;
+  disableRealtime: boolean; // NEW: Untuk mode skala besar >200 siswa
   trackLocation: boolean;
   subject: string;
   classLevel: string;
