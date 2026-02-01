95a76b12e1d00ea9be671b6b2dcd86efa441f4df
i7620-lgtm <i7620@guru.sd.belajar.id>
2026-02-01 15:24:02 +0800

    Update Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 export const LogoIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
@@ -304,4 +305,12 @@ export const UserIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
   </svg>
+);
+
+export const PrinterIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3h10.5a2.25 2.25 0 0 1 2.25 2.25v2.25H4.5V5.25A2.25 2.25 0 0 1 6.75 3ZM2.25 12.75a2.25 2.25 0 0 1 2.25-2.25h15a2.25 2.25 0 0 1 2.25 2.25v3.75a2.25 2.25 0 0 1-2.25 2.25H4.5a2.25 2.25 0 0 1-2.25-2.25v-3.75Z" />
+        <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 12.75v6.75a2.25 2.25 0 0 1-2.25 2.25H9.75a2.25 2.25 0 0 1-2.25-2.25v-6.75h9Z" />
+        <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 9.75h1.5m-12 0h1.5" />
+    </svg>
 );
