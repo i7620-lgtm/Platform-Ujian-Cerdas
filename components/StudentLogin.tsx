75ca52368c270071001def43ff7927b0d3e86a5f
i7620-lgtm <i7620@guru.sd.belajar.id>
2026-02-13 09:08:17 +0800

    Update StudentLogin.tsx

--- components/StudentLogin.tsx
+++ components/StudentLogin.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect, useRef } from 'react';
 import { ArrowLeftIcon, UserIcon, QrCodeIcon, CheckCircleIcon, LockClosedIcon } from './Icons';
 import type { Student } from '../types';
