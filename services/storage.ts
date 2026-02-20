ff039e15387dda555f095a63d185b0d2e8810df1
i7620-lgtm <i7620@guru.sd.belajar.id>
2026-02-20 09:17:25 +0800

    Update storage.ts

--- services/storage.ts
+++ services/storage.ts
@@ -1,4 +1,4 @@
-
+ 
 import { supabase } from '../lib/supabase';
 import type { Exam, Result, Question, TeacherProfile, AccountType, UserProfile, ExamSummary, ExamConfig } from '../types';
 import { compressImage } from '../components/teacher/examUtils';
