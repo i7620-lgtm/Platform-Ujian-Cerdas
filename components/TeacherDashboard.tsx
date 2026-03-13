b0a8aa136581ec1d2d70da32f3864041c4001ccb
i7620-lgtm <i7620@guru.sd.belajar.id>
2026-03-13 09:29:23 +0800

    Update TeacherDashboard.tsx

--- components/TeacherDashboard.tsx
+++ components/TeacherDashboard.tsx
@@ -454,7 +454,7 @@ export const TeacherDashboard: React.FC<TeacherDashboardProps> = ({
             )}
 
             <header className="bg-white/80 dark:bg-slate-900/80 backdrop-blur-md border-b border-slate-100 dark:border-slate-800 sticky top-0 z-40 transition-colors duration-300">
-                <div className="max-w-7xl mx-auto px-4 md:px-6">
+                <div className="w-full max-w-full mx-auto px-4 md:px-6">
                     <div className="py-3 md:py-5 flex justify-between items-center">
                         <div>
                             <div className="flex items-center gap-3">
@@ -526,7 +526,7 @@ export const TeacherDashboard: React.FC<TeacherDashboardProps> = ({
                 </div>
             </header>
             
-            <main className="max-w-7xl mx-auto p-4 md:p-10">
+            <main className="w-full max-w-full mx-auto p-4 md:p-10">
                 {view === 'UPLOAD' && (
                     <>
                         <CreationView key={resetKey} onQuestionsGenerated={handleQuestionsGenerated} />
@@ -580,7 +580,7 @@ export const TeacherDashboard: React.FC<TeacherDashboardProps> = ({
             
             {isEditModalOpen && editingExam && (
                 <div className="fixed inset-0 bg-slate-900/60 backdrop-blur-sm flex items-center justify-center p-4 z-50">
-                    <div className="bg-white dark:bg-slate-800 rounded-3xl shadow-2xl w-full max-w-5xl h-[90vh] flex flex-col border border-white dark:border-slate-700">
+                    <div className="bg-white dark:bg-slate-800 rounded-3xl shadow-2xl w-full max-w-7xl h-[90vh] flex flex-col border border-white dark:border-slate-700">
                         <div className="p-6 border-b border-slate-100 dark:border-slate-700 flex justify-between items-center bg-white dark:bg-slate-800 rounded-t-3xl">
                             <h2 className="font-black text-slate-800 dark:text-white">Edit Detail Ujian</h2>
                             <button onClick={()=>setIsEditModalOpen(false)} className="p-2 bg-slate-50 dark:bg-slate-700 text-slate-400 rounded-xl hover:bg-rose-50 hover:text-rose-600 dark:hover:bg-rose-900/30 transition-colors"><XMarkIcon className="w-6 h-6"/></button>
